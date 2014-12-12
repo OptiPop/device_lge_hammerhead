dff96033f48155c95e8e0eada5beea3b1e2c0c97
Chet Kener <Cl3Kener@gmail.com>
2014-12-12 13:16:45 -0500

    Inline Kernel Building Updates
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,11 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 maxcpus=2 msm_watchdog_v2.enable=1 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02900000 --tags_offset 0x02700000
 
+# Define kernel config for inline building
+TARGET_KERNEL_CONFIG := hammerhead_defconfig
+TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
+TARGET_GCC_VERSION_ARM := 4.9
+
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
 # Increase the size if shaders of size greater than 12KB are used.
